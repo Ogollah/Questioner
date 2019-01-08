5ec5883e4199feea7fa87d4ec7ed4b171effab27
Ogollah <kogollah2011@gmail.com>
2019-01-08 12:52:54 +0300

    [chore #163034455] Add user model

--- api/v1/main/__init__.py
+++ api/v1/main/__init__.py
@@ -3,13 +3,17 @@ This file initialize the project.
 """
 
 from flask import Flask
+from flask_bcrypt import Bcrypt
 
 # local import
 from api.v1.config import api_congig
+
+bcrypt = Bcrypt()
 
 
 def creat_app(config_name):
     api = Flask(__name__)
     api.config.from_object(api_congig[config_name])
+    bcrypt.init_app(api)
 
     return api
