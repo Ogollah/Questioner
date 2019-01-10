80865768a9f83c741543df80adf696ddcc56dd5a
Ogollah <kogollah2011@gmail.com>
2019-01-10 19:21:15 +0300

    [finishes #163036643] Add test case for signout

---
+++ api/v1/test/test_user_signout.py
@@ -0,0 +1,29 @@
+"""
+This file holds user sighout test cases.
+"""
+
+import json
+import unittest
+
+# local import
+from api.v1.test.base_test import BaseTestCase
+class UserSignoutTestCases(BaseTestCase):
+
+    def test_successfull_signout(self):
+        with self.client:
+            """
+            Test succesful signin.
+            """
+            # signup user
+            self.signup_user()
+            # signin user
+            self.signin_user()
+
+            # signout user
+            response = self.signout_user()
+            # return result in json format
+            result = json.loads(response.data.decode())
+            self.assertTrue(result['status'] == 'success')
+            self.assertTrue(result['message'] == 'You have signedout successfully.')
+            self.assertTrue(response.content_type == 'application/json')
+            self.assertEqual(response.status_code, 200)
