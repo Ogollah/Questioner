40f4932277a68ed0582032fde47f78c972d7c453
Ogollah <kogollah2011@gmail.com>
2019-01-17 09:17:43 +0300

    [Starts #163166265] Write update meetup testcase

--- api/v1/test/test_create_meetup.py
+++ api/v1/test/test_create_meetup.py
@@ -25,6 +25,17 @@ class TestCreateMeetUp(BaseTestCase):
                 hostFrom= "string"
                 )
 
+        self.meetup_data_update =dict(
+                topic= "update",
+                description= "string",
+                images= "string",
+                Tags="string",
+                createdOn= "string",
+                happeningOn= "2018-06-29 08:15:27",
+                host= "string",
+                hostFrom= "string"
+                )
+
         self.meetup_data_normal =dict(
                 topic= "Normal",
                 description= "string",
@@ -120,28 +131,26 @@ class TestCreateMeetUp(BaseTestCase):
             response = self.client.post('/api/v1/meetups/create', headers=dict(Authorization=access_token),data=json.dumps(self.meetup_data),content_type='application/json')
             # return result in json format
             result = json.loads(response.data.decode())
-            print(result)
             self.assertTrue(result['status'] == 201)
             self.assertTrue(response.content_type == 'application/json')
             self.assertEqual(response.status_code, 201)
 
-    # def test_unsuccesful_normal_user_create_meetup(self):
+    def test_unsuccesful_normal_user_create_meetup(self):
   
-    #     with self.client:
-    #         """
-    #         Test regular user cannot creat a meetup
-    #         """
-    #         self.signup_user()
-    #         resp = self.login_user()
-    #         access_token = json.loads(resp.data.decode())['access_token'] 
-    #         response = self.client.post('/api/v1/meetup/create', headers=dict(Authorization=access_token),data=json.dumps(self.meetup_data_normal),content_type='application/json')
-    #         # return result in json format
-    #         result = json.loads(response.data.decode())
-    #         print(result)
-    #         self.assertTrue(result['status'] == '401')
-    #         self.assertTrue(result['message'] == 'To create a meetup you need to be an admin for your meetup')
-    #         self.assertTrue(response.content_type == 'application/json')
-    #         self.assertEqual(response.status_code, 401)
+        with self.client:
+            """
+            Test regular user cannot creat a meetup
+            """
+            self.signup_user()
+            resp = self.login_user()
+            access_token = json.loads(resp.data.decode())['access_token'] 
+            response = self.client.post('/api/v1/meetups/create', headers=dict(Authorization=access_token),data=json.dumps(self.meetup_data_normal),content_type='application/json')
+            # return result in json format
+            result = json.loads(response.data.decode())
+            self.assertTrue(result['status'] == 401)
+            self.assertTrue(result['message'] == 'To create a meetup you need to be an admin for your meetup')
+            self.assertTrue(response.content_type == 'application/json')
+            self.assertEqual(response.status_code, 401)
 
 
     def test_succesful_crete_meetup_with_no_image(self):
@@ -207,6 +216,37 @@ class TestCreateMeetUp(BaseTestCase):
             self.assertTrue(result['status'] == 409)
             self.assertTrue(response.content_type == 'application/json')
             self.assertEqual(response.status_code, 409)
+
+    def test_update_unavailable_meetup(self):
+
+        with self.client:
+            """
+            Test can not update unavaible meetup
+            """
+            
+            resp = self.login_user_admin()
+            access_token = json.loads(resp.data.decode())['access_token'] 
+            response = self.client.patch('/api/v1/meetups/7/update', headers=dict(Authorization=access_token),data=json.dumps(self.meetup_data_update),content_type='application/json')
+            result = json.loads(response.data.decode())
+            self.assertTrue(result['status'] == 404)
+            self.assertTrue(response.content_type == 'application/json')
+            self.assertEqual(response.status_code, 404)
+
+    def test_update_normal_user_meetup(self):
+
+        with self.client:
+            """
+            Test normal user cannot update a meetup
+            """
+            
+            self.signup_user()
+            resp = self.login_user()
+            access_token = json.loads(resp.data.decode())['access_token'] 
+            response = self.client.patch('/api/v1/meetups/1/update', headers=dict(Authorization=access_token),data=json.dumps(self.meetup_data_update),content_type='application/json')
+            result = json.loads(response.data.decode())
+            self.assertTrue(result['status'] == 401)
+            self.assertTrue(response.content_type == 'application/json')
+            self.assertEqual(response.status_code, 401)
 
 if __name__ == '__main__':
     unittest.main()
